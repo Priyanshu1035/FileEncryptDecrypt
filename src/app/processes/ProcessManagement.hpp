0748e363555c336e7b52c4d112b0f2bfa7b1c2f3
Priyanshu1035 <priyanshukhawas18@gmail.com>
2025-09-10 12:25:05 +0530

    Added multiprocessing/child process logic

--- src/app/processes/ProcessManagement.hpp
+++ src/app/processes/ProcessManagement.hpp
@@ -4,14 +4,35 @@
 #include "Task.hpp"
 #include<queue>
 #include<memory>
+#include<semaphore.h>
+#include<mutex>
 
 class ProcessManagement{
+    
+    sem_t* itemsSemaphore;
+    sem_t* emptySlotsSemaphore;
+
     public:
         ProcessManagement();
+        ~ProcessManagement();
         bool submitToQueue(std::unique_ptr<Task>  task);
         void executeTasks();
     private:
-        std::queue<std::unique_ptr<Task>> taskQueue;
+        struct SharedMemory{
+            std::atomic<int> size;
+            char tasks[1000][256];
+            int front;
+            int rear;
+
+            void printSharedMemory(){
+                std::cout<<size<<std::endl;
+            }
+        };
+        SharedMemory* sharedMem;
+        int shmFd;
+        const char* SHM_NAME = "/my_queue";
+        std::mutex queueLock;
+
 }; 
 
 #endif
